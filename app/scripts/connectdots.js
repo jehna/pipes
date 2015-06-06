6f178a1fc909324ecb5f07049376bd47bff601d7
jehna <jesse.luoto@gmail.com>
2015-06-06 23:34:08 +0300

    Fixed: Input bezire easing

--- app/scripts/connectdots.js
+++ app/scripts/connectdots.js
@@ -56,6 +56,7 @@ $(function() {
   var canvas = $canvas[0];
   var ctx = new RetinaCanvas(canvas.getContext('2d'));
   var begin;
+  var swap;
   
   ctx.imageSmoothingEnabled = true;
   
@@ -64,37 +65,53 @@ $(function() {
   
   var enableDrag = function(event) {
     
-    var diff = {
-      x: event.pageX - begin.eX,
-      y: event.pageY - begin.eY,
-    };
-    
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.beginPath();
-    ctx.lineWidth = 6;
-    ctx.strokeStyle = '#ffcc00';
-    ctx.moveTo(begin.eX,begin.eY);
-    
-    var point1 = {
-      x: begin.eX + (diff.x * 0.5),
+    var from = {
+      x: begin.eX,
       y: begin.eY
     };
     
-    var point2 = {
-      x: begin.eX + (diff.x * 0.5),
+    var to = {
+      x: event.pageX,
       y: event.pageY
+    };
+    
+    if (swap) {
+      var tmp = from;
+      from = to;
+      to = tmp;
+    }
+    
+    var diff = {
+      x: to.x - from.x,
+      y: to.y - from.y,
+    };
+    
+    ctx.clearRect(0, 0, canvas.width/2, canvas.height/2);
+    ctx.beginPath();
+    ctx.lineWidth = 6;
+    ctx.strokeStyle = '#ffcc00';
+    ctx.moveTo(from.x,from.y);
+    
+    var point1 = {
+      x: from.x + (diff.x * 0.5),
+      y: from.y
+    };
+    
+    var point2 = {
+      x: from.x + (diff.x * 0.5),
+      y: to.y
     };
     
     var MIN_DIFF = 50;
     if (diff.x < MIN_DIFF) {
-      point1.x = begin.eX + MIN_DIFF/2 + ((diff.x-MIN_DIFF)*-0.4);
-      point2.x = event.pageX - MIN_DIFF/2 + ((diff.x-MIN_DIFF)*0.4);
+      point1.x = from.x + MIN_DIFF/2 + ((diff.x-MIN_DIFF)*-0.4);
+      point2.x = to.x - MIN_DIFF/2 + ((diff.x-MIN_DIFF)*0.4);
     }
     
     ctx.bezierCurveTo(
       point1.x, point1.y,
       point2.x, point2.y,
-      event.pageX, event.pageY
+      to.x, to.y
     );
     ctx.stroke();
   };
@@ -110,6 +127,9 @@ $(function() {
     var type = $t.is('.input') ? INPUT : OUTPUT;
     
     if (event.type === 'mousedown') {
+      
+      swap = type === INPUT;
+      
       begin = {
         element: $t,
         eX: $t.offset().left + (type === INPUT ? -20 : ($t.width() + 20)),
